84ec085681a4d7f94b08e904dcbd754e9b686e8b
daeho <mnisdh2@gmail.com>
2023-05-18 13:21:26 +0900

    23 computed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="container">
+    <h4>count: {{ count }}</h4>
+    <h4>double count computed: {{ doubleCountComputed }}</h4>
+    <h4>double count computed: {{ doubleCountComputed }}</h4>
+    <h4>double count method: {{ doubleCountMethod() }}</h4>
+    <h4>double count method: {{ doubleCountMethod() }}</h4>
+    <button @click="count++">Add one</button>
     <h2>To-Do List</h2>
     <TodoSimpleForm @add-todo="addTodo" />
 
@@ -15,7 +21,7 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -48,6 +54,17 @@ export default {
       toggle.value = !toggle.value;
     };
 
+    const count = ref(1);
+    const doubleCountComputed = computed(() => {
+      console.log("computed");
+      return count.value * 2;
+    });
+
+    const doubleCountMethod = () => {
+      console.log("method");
+      return count.value * 2;
+    }
+
     return {
       toggle,
       todos,
@@ -56,6 +73,9 @@ export default {
       onToggle,
       todoStyle,
       deleteTodo,
+      count,
+      doubleCountComputed,
+      doubleCountMethod,
     };
   }
 }
