5a87282af23da9011d4038582b3fc07bb8b1d511
sdh <mnisdh@forcewave.co.kr>
2023-05-18 10:10:06 +0900

    17 폼 컴포넌트 만들기

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,6 @@
   <div class="container">
     <h2>To-Do List</h2>
     <todoSimpleForm />
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control"
-            type="text" 
-            v-model="todo"
-            placeholder="Type new to-do"
-          >
-        </div>
-        <div>
-          <button
-            class="btn btn-primary"
-            type="submit"
-          >
-          Add
-          </button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red">
-        This field cannot be empty
-      </div>
-    </form>
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
