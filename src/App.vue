27faa4b3b8195a8f3cc1ca70cd8c3218c51f648f
daeho <mnisdh2@gmail.com>
2023-05-18 11:28:04 +0900

    19 props

--- src/App.vue
+++ src/App.vue
@@ -2,33 +2,24 @@
   <div class="container">
     <h2>To-Do List</h2>
     <TodoSimpleForm @add-todo="addTodo" />
+
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
-    <div
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
-    >
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input class="form-check-input" type="checkbox" v-model="todo.completed">
-          <label class="form-check-label" :class="{todo: todo.completed}">{{ todo.subject }}</label>
-        </div>
-        <div>
-          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
-        </div>
-      </div>
-    </div>
+    <TodoList :todos="todos" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
-  components: { TodoSimpleForm },
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
   setup() {
     const toggle = ref(false);
     const todos = ref([]);
