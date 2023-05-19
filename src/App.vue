88b6e20761c2309c2637c3286371b1da2e87ec49
daeho <mnisdh2@gmail.com>
2023-05-19 10:40:40 +0900

    31 To-Do 토글 db에서 하기

--- src/App.vue
+++ src/App.vue
@@ -70,8 +70,18 @@ export default {
       }
     };
 
-    const toggleTodo = (index) => {
-      todos.value[index].completed = !todos.value[index].completed;
+    const toggleTodo = async (index) => {
+      error.value = "";
+      const id = todos.value[index].id;
+      try {
+        await axios.patch("http://localhost:3000/todos/" + id, {
+          completed: !todos.value[index].completed
+        });
+        todos.value[index].completed = !todos.value[index].completed;
+      } catch(err) {
+        console.log(err);
+        error.value = "Somthing wen wrong";
+      }
     };
 
     const deleteTodo = async (index) => {
