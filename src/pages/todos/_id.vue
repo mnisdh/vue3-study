2d28607f6c2e8e3a633ea2ccc97151c75db52991
daeho <mnisdh2@gmail.com>
2023-05-22 13:08:55 +0900

    45 To-Do 수정 페이지 route 추가

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Todo page</div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+
+export default {
+  setup() {
+    const route = useRoute();
+
+    console.log(route.params);
+  }
+}
+</script>
+
+<style>
+
+</style>
