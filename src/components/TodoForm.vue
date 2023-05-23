a87721c8d7bfd19358689758c0e37423822cfa1a
daeho <mnisdh2@gmail.com>
2023-05-23 09:33:00 +0900

    60 폼 validation

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,67 +1,72 @@
 <template>
-  <div v-if="loading">
-    Loading....
-  </div>
-  <form
-    v-else
-    @submit.prevent="onSave"
-  >
-    <div class="row">
-      <div class="col-6">
-        <div class="form-group">
-          <label>Subject</label>
-          <input
-            v-model="todo.subject"
-            type="text"
-            class="form-control"
-          >
-        </div>
-      </div>
-      <div v-if="editing" class="col-6">
-        <div class="form-group">
-          <label>Status</label>
-          <div>
-            <button
-              class="btn"
-              type="button"
-              :class="todo.completed ? 'btn-success' : 'btn-danger'"
-              @click="toggleTodoStatus"
-            >
-              {{ todo.completed ? 'Completed' : 'Incomplete' }}
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="col-12">
-        <div class="form-group">
-          <label>Body</label>
-          <textarea v-model="todo.body" class="form-control" cols="30" rows="10">
-            
-          </textarea>
-        </div>
-      </div>
+    <div v-if="loading">
+        Loading....
     </div>
+    <form
+        v-else
+        @submit.prevent="onSave"
+    >
+        <div class="row">
+            <div class="col-6">
+                <div class="form-group">
+                    <label>Subject</label>
+                    <input
+                        v-model="todo.subject"
+                        type="text"
+                        class="form-control"
+                    >
+                    <div
+                        v-if="subjectError"
+                        style="color: red"
+                    >
+                        {{ subjectError }}
+                    </div>
+                </div>
+            </div>
+            <div v-if="editing" class="col-6">
+                <div class="form-group">
+                    <label>Status</label>
+                    <div>
+                        <button
+                            class="btn"
+                            type="button"
+                            :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                            @click="toggleTodoStatus"
+                        >
+                            {{ todo.completed ? 'Completed' : 'Incomplete' }}
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="form-group">
+                    <label>Body</label>
+                    <textarea v-model="todo.body" class="form-control" cols="30" rows="10">
+                    </textarea>
+                </div>
+            </div>
+        </div>
     
-    <button
-      type="submit"
-      class="btn btn-primary"
-      :disabled="!todoUpdated"
-    >
-      {{ editing ? 'Update' : 'Create' }}
-    </button>
-    <button
-      type="submit"
-      class="btn btn-outline-dark ml-2"
-      @click="moveToTodoListPage"
-    >
-      Cancel
-    </button>
-  </form>
-  <Toast
-    v-if="showToast"
-    :message="toastMessage"
-    :type="toastAlertType"
-  />
+        <button
+            type="submit"
+            class="btn btn-primary"
+            :disabled="!todoUpdated"
+        >
+            {{ editing ? 'Update' : 'Create' }}
+        </button>
+        <button
+            type="submit"
+            class="btn btn-outline-dark ml-2"
+            @click="moveToTodoListPage"
+        >
+            Cancel
+        </button>
+    </form>
+    <Toast
+        v-if="showToast"
+        :message="toastMessage"
+        :type="toastAlertType"
+    />
 </template>
 
 <script>
@@ -90,6 +95,7 @@ export default {
         completed: false,
         body: ''
     });
+    const subjectError = ref('');
     const originalTodo = ref(null);
     const loading = ref(false);
     const {
@@ -135,29 +141,35 @@ export default {
     }
 
     const onSave = async () => {
-      try {
-        let res;
-        const data = {
-            subject: todo.value.subject,
-            completed: todo.value.completed,
-            body: todo.value.body
-        };
-
-        if(props.editing) {
-            res = await axios.put(`http://localhost:3000/todos/${todoId}`, data);
-            originalTodo.value = { ...res.data };
-        } else {
-            res = await axios.post(`http://localhost:3000/todos`, data);
-            todo.value.subject = '';
-            todo.value.body = '';
+        subjectError.value = '';
+        if(!todo.value.subject) {
+            subjectError.value = 'Subject is required';
+            return;
         }
-        
-        const message = `Successfully ${props.editing?'Updated':'Created'}!`;
-        triggerToast(message);
-      } catch(error) {
-        console.log(error);
-        triggerToast('Somthing went wrong', 'danger');
-      }
+    
+        try {
+            let res;
+            const data = {
+                subject: todo.value.subject,
+                completed: todo.value.completed,
+                body: todo.value.body
+            };
+
+            if(props.editing) {
+                res = await axios.put(`http://localhost:3000/todos/${todoId}`, data);
+                originalTodo.value = { ...res.data };
+            } else {
+                res = await axios.post(`http://localhost:3000/todos`, data);
+                todo.value.subject = '';
+                todo.value.body = '';
+            }
+            
+            const message = `Successfully ${props.editing?'Updated':'Created'}!`;
+            triggerToast(message);
+        } catch(error) {
+            console.log(error);
+            triggerToast('Somthing went wrong', 'danger');
+        }
     }
 
     return {
@@ -170,6 +182,7 @@ export default {
       showToast,
       toastMessage,
       toastAlertType,
+      subjectError,
     };
   }
 }
