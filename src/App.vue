5d24ee28c5adb1a7baa44ce3e9026ded60713eba
daeho <mnisdh2@gmail.com>
2023-05-23 17:12:19 +0900

    74 Toast 컴포넌트 App 컴포넌트로 이동

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,36 @@
   <div class="container">
     <router-view/>
   </div>
+  <Toast
+    v-if="showToast"
+    :message="toastMessage"
+    :type="toastAlertType"
+  />
 </template>
 
 <script>
+import Toast from '@/components/Toast.vue';
+import { useToast } from '@/composables/toast';
+
 export default {
-  
+  components: {
+    Toast,
+  },
+  setup() {
+    const {
+      showToast,
+      toastMessage,
+      toastAlertType,
+      triggerToast
+    } = useToast();
+
+    return {
+      showToast,
+      toastMessage,
+      toastAlertType,
+      triggerToast,
+    }
+  }
 }
 </script>
 
