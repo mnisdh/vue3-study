a4f8f5343f1c21bfa07f0f49fe649c9ad8dfb58a
daeho <mnisdh2@gmail.com>
2023-05-23 09:15:11 +0900

    58 To-Do 생성 페이지 만들기 1

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,169 @@
+<template>
+  <div v-if="loading">
+    Loading....
+  </div>
+  <form
+    v-else
+    @submit.prevent="onSave"
+  >
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input
+            v-model="todo.subject"
+            type="text"
+            class="form-control"
+          >
+        </div>
+      </div>
+      <div v-if="editing" class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button
+              class="btn"
+              type="button"
+              :class="todo.completed ? 'btn-success' : 'btn-danger'"
+              @click="toggleTodoStatus"
+            >
+              {{ todo.completed ? 'Completed' : 'Incomplete' }}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="form-group">
+          <label>Body</label>
+          <textarea v-model="todo.body" class="form-control" cols="30" rows="10">
+            
+          </textarea>
+        </div>
+      </div>
+    </div>
+    
+    <button
+      type="submit"
+      class="btn btn-primary"
+      :disabled="!todoUpdated"
+    >
+      Save
+    </button>
+    <button
+      type="submit"
+      class="btn btn-outline-dark ml-2"
+      @click="moveToTodoListPage"
+    >
+      Cancel
+    </button>
+  </form>
+  <Toast
+    v-if="showToast"
+    :message="toastMessage"
+    :type="toastAlertType"
+  />
+</template>
+
+<script>
+import { useRoute, useRouter } from 'vue-router';
+import axios from 'axios';
+import { ref, computed } from 'vue';
+import _ from 'lodash';
+import Toast from '@/components/Toast.vue';
+import { useToast } from '@/composables/toast';
+
+export default {
+  components: {
+    Toast,
+  },
+  props: {
+    editing: {
+        type: Boolean,
+        default: false
+    }
+  },
+  setup(props) {
+    const route = useRoute();
+    const router = useRouter();
+    const todo = ref({
+        subject: '',
+        completed: false,
+        body: ''
+    });
+    const originalTodo = ref(null);
+    const loading = ref(false);
+    const {
+      showToast,
+      toastMessage,
+      toastAlertType,
+      triggerToast
+    } = useToast();
+    const todoId = route.params.id;
+
+    const getTodo = async () => {
+      try {
+        loading.value = true;
+        const res = await axios.get(`http://localhost:3000/todos/${todoId}`);
+        
+        todo.value = { ...res.data };
+        originalTodo.value = { ...res.data };
+
+        loading.value = false;
+      } catch(error) {
+        console.log(error);
+        triggerToast('Somthing went wrong', 'danger');
+        loading.value = false;
+      }
+    };
+
+    const todoUpdated = computed(() => {
+      return !_.isEqual(todo.value, originalTodo.value);
+    });
+
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed;
+    };
+
+    const moveToTodoListPage = () => {
+      router.push({
+        name: 'Todos'
+      });
+    };
+    
+    if(props.editing) {
+        getTodo();
+    }
+
+    const onSave = async () => {
+      try {
+        const res = await axios.put(`http://localhost:3000/todos/${todoId}`,{
+          subject: todo.value.subject,
+          completed: todo.value.completed
+        });
+
+        originalTodo.value = { ...res.data };
+        triggerToast('Successfully saved!');
+      } catch(error) {
+        console.log(error);
+        triggerToast('Somthing went wrong', 'danger');
+      }
+    }
+
+    return {
+      todo,
+      loading,
+      toggleTodoStatus,
+      moveToTodoListPage,
+      onSave,
+      todoUpdated,
+      showToast,
+      toastMessage,
+      toastAlertType,
+    };
+  }
+}
+</script>
+
+<style>
+
+</style>
