aafbfd30f2ebc856b9c8a52153754d3b1a5d7199
daeho <mnisdh2@gmail.com>
2023-05-24 10:16:35 +0900

    80 transition-group

--- src/App.vue
+++ src/App.vue
@@ -11,58 +11,19 @@
   <div class="container">
     <router-view/>
   </div>
-  <transition name="slide">
-    <Toast
-      v-if="showToast"
-      :message="toastMessage"
-      :type="toastAlertType"
-    />
-  </transition>
+  <Toast />
 </template>
 
 <script>
 import Toast from '@/components/Toast.vue';
-import { useToast } from '@/composables/toast';
 
 export default {
   components: {
     Toast,
-  },
-  setup() {
-    const {
-      showToast,
-      toastMessage,
-      toastAlertType,
-      triggerToast
-    } = useToast();
-
-    console.log(showToast.value);
-
-    return {
-      showToast,
-      toastMessage,
-      toastAlertType,
-      triggerToast,
-    }
   }
 }
 </script>
 
-<style scoped>
-  .slide-enter-active,
-  .slide-leave-active {
-      transition: all 0.5s ease;
-  }
-
-  .slide-enter-from,
-  .slide-leave-to {
-      opacity: 0;
-      transform: translateY(-30px);
-  }
-
-  .slide-enter-to,
-  .slide-leave-from {
-      opacity: 1;
-      transform: translateY(0px);
-  }
+<style>
+  
 </style>
--- src/store/modules/toast/index.js
+++ src/store/modules/toast/index.js
@@ -1,32 +1,27 @@
 export default {
     namespaced: true,
     state: {
-        toastMessage: '',
-        toastAlertType: '',
-        showToast: false
+        toasts: []
     },
     mutations: {
-        UPDATE_TOAST_MESSAGE (state, payload) {
-            state.toastMessage = payload;
+        ADD_TOAST (state, payload) {
+            state.toasts.push(payload);
         },
-        UPDATE_TOAST_ALERT_TYPE (state, payload) {
-            state.toastAlertType = payload;
-        },
-        UPDATE_TOAST_STATUS (state, payload) {
-            state.showToast = payload;
+        REMOVE_TOAST (state) {
+            state.toasts.shift();
         }
     },
     actions: {
         triggerToast({ commit }, message, type = 'success') {
-            commit('UPDATE_TOAST_MESSAGE', message);
-            commit('UPDATE_TOAST_ALERT_TYPE', type);
-            commit('UPDATE_TOAST_STATUS', true);
+            commit('ADD_TOAST', {
+                id: Date.now(),
+                message,
+                type,
+            });
 
             setTimeout(() => {
-                commit('UPDATE_TOAST_MESSAGE', '');
-                commit('UPDATE_TOAST_ALERT_TYPE', '');
-                commit('UPDATE_TOAST_STATUS', false);
-            }, 3000);
+                commit('REMOVE_TOAST');
+            }, 5000);
         }
     },
     getters: {
